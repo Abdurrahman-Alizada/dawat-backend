0c3e39c15c2477b4193e07b6c37b1ea0b98c9c5d
Abdurrahman-Alizada <abdurrahmanalizada17@gmail.com>
2023-06-20 18:45:26 +0500

    task and invitaions summary

--- router/invitationRouter.js
+++ router/invitationRouter.js
@@ -8,13 +8,15 @@ import {
   updateInvitiStatus,
   createMultipleInviti,
   deleteMultipleInviti,
-  updateStatusOfMultipleInvities
+  updateStatusOfMultipleInvities,
+  invitiesSummary
 } from "../controllers/invitationController.js";
 import protect from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/:groupId").get(protect, allInvities);
+router.route("/:groupId/invitiesSummary").get(protect, invitiesSummary);
 router.route("/").post(protect, createInviti);
 router.route("/addMultiple").post(protect, createMultipleInviti);
 router.route("/delete").delete(protect, deleteInviti);
--- router/taskRouter.js
+++ router/taskRouter.js
@@ -12,12 +12,14 @@ import {
   updateStartingDates,
   updatePriority,
   updateResponsibles,
+  tasksSummary
 } from "../controllers/taskController.js";
 import protect from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/:groupId").get(protect, allTasks);
+router.route("/:groupId/tasksSummary").get(protect, tasksSummary);
 router.route("/").post(protect, createTask);
 router.route("/delete").delete(protect, deleteTask);
 router.route("/update").put(protect, updateTask);
